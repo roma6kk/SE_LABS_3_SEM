190cf1f6aff1ff61953cf2b46120d2afff22aa1f
Roman <123767751+roma6kk@users.noreply.github.com>
2024-11-21 15:01:42 +0300

    Add files via upload

---
+++ SE_ASM01/SE_Asm01.asm
@@ -0,0 +1,28 @@
+.586
+.MODEL FLAT, STDCALL
+includelib kernel32.lib
+
+ExitProcess PROTO : DWORD
+MessageBoxA PROTO : DWORD, : DWORD, : DWORD, : DWORD
+
+.STACK 4096
+
+.CONST
+
+.DATA
+MB_OK EQU 0
+STR1 DB "Моя первая программа", 0
+STR2 DB "Привет всем!", 0
+HW DD ?
+
+.CODE
+
+main PROC
+START:
+		INVOKE MessageBoxA, HW, OFFSET STR2, OFFSET STR1, MB_OK
+
+	push 0
+	call ExitProcess
+main ENDP
+
+end main
